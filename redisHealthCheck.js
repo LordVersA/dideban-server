1800b202960b837d4fd30078ef14a3b21c71edf5
Parsa Radfar <parsa.plus@gmail.com>
2023-03-25 09:30:22 +0430

    fix redis password

--- redisHealthCheck.js
+++ redisHealthCheck.js
@@ -3,9 +3,11 @@ const { exec } = require("child_process");
 
 const checkRedisHealth = async () => {
     const message = { enable: true, connection: false, query: false };
+    const redisPassword = process.env.REDIS_PASSWORD;
+    const redisCliCommand = redisPassword ? `redis-cli -a ${redisPassword} ping` : "redis-cli ping";
 
     try {
-        const redisCliPing = promisify(exec)("redis-cli ping");
+        const redisCliPing = promisify(exec)(redisCliCommand);
         const { stdout } = await redisCliPing;
         message.connection = true;
         if (stdout.trim() === "PONG") {
@@ -18,4 +20,5 @@ const checkRedisHealth = async () => {
         return error;
     }
 };
+
 module.exports = checkRedisHealth;
