518496220bbd90aa4409f27346e28afc748f42b0
Parsa Radfar <parsa.plus@gmail.com>
2023-05-09 12:13:10 +0330

    fix tcp

--- TCPconnections.js
+++ TCPconnections.js
@@ -5,13 +5,15 @@ const checkTCPConnections = async () => {
     const message = { enable: true, netstats_installed: false, established_count: null };
 
     try {
-        const firewalldPing = promisify(exec)("netstat -ant | grep -i 'established' | grep ':80 |:443 ' | wc -l");
-        const { stdout, stderr, error } = await firewalldPing;
+        const tcpPing = promisify(exec)("netstat -ant | grep -i 'established' | grep ':80 |:443 ' | wc -l");
+        const { stdout, stderr, error } = await tcpPing;
+        return { stdout, stderr, error };
         if ((error = error + stderr)) {
             if (!error.includes("command not found")) {
                 message.netstats_installed = true;
+            } else {
+                return message;
             }
-            return message;
         }
         message.established_count = stdout;
 
